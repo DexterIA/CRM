8ce404333b530dd35cedb53a5d8ce1605aaacf87
DexterIA <dexterrrrria@gmail.com>
2016-05-15 18:53:58 +0300

    added login form

---
+++ simpleFace/js/main.js
@@ -0,0 +1,33 @@
+angular.module('CRM', ['ngMaterial', 'ngMessages', 'loginPage'])
+  .controller('main', ['$scope', '$mdDialog', '$mdMedia',
+    function ($scope, $mdDialog, $mdMedia) {
+      $scope.status = '  ';
+      $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+
+      $scope.showAdvanced = function (ev) {
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+
+        $mdDialog.show({
+            controller: 'loginPageCtrl',
+            templateUrl: 'templates/loginPage.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: useFullScreen
+          })
+          .then(function (answer) {
+            $scope.status = 'You said the information was "' + answer + '".';
+          }, function () {
+            $scope.status = 'You cancelled the dialog.';
+          });
+
+
+        $scope.$watch(function () {
+          return $mdMedia('xs') || $mdMedia('sm');
+        }, function (wantsFullScreen) {
+          $scope.customFullscreen = (wantsFullScreen === true);
+        });
+
+      };
+
+    }]);
