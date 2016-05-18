b64505c841e92504441422274bbd78f85ef5a0fb
DexterIA <dexterrrrria@gmail.com>
2016-05-18 09:26:20 +0300

    добавил информацию о клиенте и его заказах пока без темизации

--- simpleFace/js/main.js
+++ simpleFace/js/main.js
@@ -1,6 +1,6 @@
 angular.module('CRM', ['ngMaterial', 'ngMessages', 'loginPage'])
-  .controller('main', ['$scope', '$mdDialog', '$mdMedia',
-    function ($scope, $mdDialog, $mdMedia) {
+  .controller('main', ['$scope', '$mdDialog', '$mdMedia', '$http',
+    function ($scope, $mdDialog, $mdMedia, $http) {
       $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
 
       /**
@@ -9,6 +9,8 @@ angular.module('CRM', ['ngMaterial', 'ngMessages', 'loginPage'])
        */
       $scope.showLoginDialog = function (ev) {
         var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+        $scope.login = null;
+        $scope.showInfo = false;
 
         $mdDialog.show({
             controller: 'loginPageCtrl',
@@ -20,6 +22,7 @@ angular.module('CRM', ['ngMaterial', 'ngMessages', 'loginPage'])
           })
           .then(function (answer) {
             $scope.login = answer;
+            $scope.loadCustomer();
           });
 
 
@@ -29,6 +32,16 @@ angular.module('CRM', ['ngMaterial', 'ngMessages', 'loginPage'])
           $scope.customFullscreen = wantsFullScreen === true;
         });
 
+      };
+
+      $scope.loadCustomer = function () {
+        var auth = $http.post('http://127.0.0.1:8081/CRM/findCustomer', {login: $scope.login});
+        auth.success(function(data) {
+          if (data && data[0]){
+            $scope.customer = data[0];
+            $scope.showInfo = true;
+          }
+        });
       };
 
     }]);
